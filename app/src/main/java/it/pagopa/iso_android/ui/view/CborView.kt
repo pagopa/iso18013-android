6b8e5dfa37b6f813c59b697a0aade35c7e9686d7
Alberto Antonino Cuffaro <alberto.cuffaro@digitouch.it>
2024-10-01 09:57:08 +0200

    add ui for test

--- app/src/main/java/it/pagopa/iso_android/ui/view/CborView.kt
+++ app/src/main/java/it/pagopa/iso_android/ui/view/CborView.kt
@@ -1,11 +1,12 @@
 package it.pagopa.iso_android.ui.view
 
 import android.content.res.Configuration
+import android.graphics.BitmapFactory
 import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -23,6 +24,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import it.pagopa.cbor_implementation.impl.MDoc
@@ -75,35 +78,52 @@ fun CborView(
                 ?.forEach {
                     it?.keys?.forEach { key ->
                         item {
-                            Text(text = key)
+                            Text(
+                                modifier = Modifier
+                                    .padding(top = 16.dp),
+                                text = key,
+                                color = Color.Black
+                            )
                         }
                         items(
                             items = it[key].orEmpty(),
                             itemContent = { item ->
-                                Row(
+                                Column(
                                     modifier = Modifier
+                                        .padding(top = 16.dp)
                                         .fillMaxWidth()
-                                        .padding(vertical = 8.dp),
-                                    verticalAlignment = Alignment.CenterVertically
+                                        .background(Color.LightGray)
+                                        .padding(8.dp),
                                 ) {
                                     Text(
                                         modifier = Modifier
-                                            .weight(1f),
-                                        text = item.elementIdentifier
-                                    )
-                                    Text(
-                                        modifier = Modifier
-                                            .weight(1f),
-                                        text = item.elementValue.toString()
+                                            .fillMaxWidth(),
+                                        text = "${item.elementIdentifier} (${item.elementValue?.javaClass?.name})"
                                     )
 
+                                    if (item.elementIdentifier == "portrait") {
+                                        Image(
+                                            modifier = Modifier
+                                                .padding(top = 16.dp)
+                                                .fillMaxWidth(),
+                                            bitmap = BitmapFactory.decodeByteArray(item.elementValue as ByteArray, 0, (item.elementValue as ByteArray).size)
+                                                .asImageBitmap(),
+                                            contentDescription = null
+                                        )
+                                    } else {
+                                        Text(
+                                            modifier = Modifier
+                                                .padding(top = 16.dp)
+                                                .fillMaxWidth(),
+                                            text = item.elementValue.toString()
+                                        )
+                                    }
                                 }
                             }
                         )
                     }
                 }
         }
-
     }
 }
 
