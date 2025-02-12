7ac646c7204ee41938d92f1657a23e707ed33ca5
hevelius <mario.perrotta@pagopa.it>
2025-02-12 16:32:07 +0100

    chore: update license ref

--- proximity/build.gradle.kts
+++ proximity/build.gradle.kts
@@ -51,8 +51,8 @@ android {
 
                     licenses {
                         license {
-                            name.set("The Apache License, Version 2.0")
-                            url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                            name.set("MIT License")
+                            url.set("https://github.com/pagopa/iso18013-android/blob/main/LICENSE")
                         }
                     }
 
