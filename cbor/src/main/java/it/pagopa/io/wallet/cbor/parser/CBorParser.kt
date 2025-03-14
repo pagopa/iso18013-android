bf490963cd68a978f7b6b9c7c5ed67274b52cc9f
carloDigitouch <156417387+carloDigitouch@users.noreply.github.com>
2025-03-14 08:32:34 +0000

    added commetns as requested

--- cbor/src/main/java/it/pagopa/io/wallet/cbor/parser/CBorParser.kt
+++ cbor/src/main/java/it/pagopa/io/wallet/cbor/parser/CBorParser.kt
@@ -9,6 +9,8 @@ import org.json.JSONObject
 import kotlin.io.encoding.Base64
 import kotlin.io.encoding.ExperimentalEncodingApi
 
+/**
+ * Class to parse cbor raw value to json string, it accepts both bytes[] value or Base64 string*/
 class CBorParser private constructor(
     private val source: Any,
     private val isByteArray: Boolean = false
@@ -16,6 +18,8 @@ class CBorParser private constructor(
     constructor(source: String) : this(source, false)
     constructor(source: ByteArray) : this(source, true)
 
+    /**
+     * It parses a Cbor raw value to a json string without any other intervention as [documentsCborToJson] or [issuerSignedCborToJson]*/
     @OptIn(ExperimentalEncodingApi::class)
     fun toJson(): String? {
         return try {
@@ -28,6 +32,23 @@ class CBorParser private constructor(
         }
     }
 
+
+    /**
+     * It parses a document Cbor raw value to a json string
+     * @param separateElementIdentifier false if you want to merge elementIdentifier and elementValue as i.e.:
+     * {
+     *   "random": "749knalJ0xgtGBK1lVhhXe8D-beFtIiXyln1UhmTPKmbNmTwvolOya3h-_AyFE2MqCom3sBYs8VylU238nkOTA==",
+     *   "digestID": 14,
+     *   "family_name": "ANDERSSON"
+     * } else i.e.:
+     * {
+     *   "random": "749knalJ0xgtGBK1lVhhXe8D-beFtIiXyln1UhmTPKmbNmTwvolOya3h-_AyFE2MqCom3sBYs8VylU238nkOTA==",
+     *   "digestID": 14,
+     *   "elementIdentifier": "family_name",
+     *   "elementValue": "ANDERSSON"
+     * }
+     * @param onComplete callback with the json string
+     * @param onError callback with the error as [Exception]*/
     fun documentsCborToJson(
         separateElementIdentifier: Boolean = true,
         onComplete: (String) -> Unit,
@@ -44,7 +65,21 @@ class CBorParser private constructor(
             onComplete.invoke(model.toJson().toString())
         }, onError = onError ?: {})
     }
-
+    /**
+     * As [documentsCborToJson] It parses a issuerSigned Cbor raw value to a json string
+     * @param separateElementIdentifier false if you want to merge elementIdentifier and elementValue as i.e.:
+     * {
+     *   "random": "749knalJ0xgtGBK1lVhhXe8D-beFtIiXyln1UhmTPKmbNmTwvolOya3h-_AyFE2MqCom3sBYs8VylU238nkOTA==",
+     *   "digestID": 14,
+     *   "family_name": "ANDERSSON"
+     * } else i.e.:
+     * {
+     *   "random": "749knalJ0xgtGBK1lVhhXe8D-beFtIiXyln1UhmTPKmbNmTwvolOya3h-_AyFE2MqCom3sBYs8VylU238nkOTA==",
+     *   "digestID": 14,
+     *   "elementIdentifier": "family_name",
+     *   "elementValue": "ANDERSSON"
+     * }
+     * @return json string or null if error*/
     @OptIn(ExperimentalEncodingApi::class)
     fun issuerSignedCborToJson(
         separateElementIdentifier: Boolean = true
