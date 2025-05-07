cbb4ede640b424cd121ea63baed36647a4f13dbd
LazyAfternoons <lazyafternoons@outlook.it>
2025-05-07 17:26:43 +0200

    chore: Release cbor v1.2.4

--- cbor/build.gradle.kts
+++ cbor/build.gradle.kts
@@ -36,7 +36,7 @@ android {
 }
 
 mavenPublishing {
-    coordinates("it.pagopa.io.wallet.cbor", "cbor", "1.2.3")
+    coordinates("it.pagopa.io.wallet.cbor", "cbor", "1.2.4")
 
     publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
     signAllPublications()
