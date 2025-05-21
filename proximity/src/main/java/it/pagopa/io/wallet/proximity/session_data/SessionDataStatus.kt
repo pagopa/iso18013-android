61a6f291af49b807818d74f74f7099ef302eca0b
Carlo <carlo.dechellis@digitouch.it>
2025-05-21 15:21:59 +0200

    commented SessionDataStatus

--- proximity/src/main/java/it/pagopa/io/wallet/proximity/session_data/SessionDataStatus.kt
+++ proximity/src/main/java/it/pagopa/io/wallet/proximity/session_data/SessionDataStatus.kt
@@ -1,5 +1,14 @@
 package it.pagopa.io.wallet.proximity.session_data
-
+/**
+**Table 20 — SessionData status codes**
+__________________________________________________________________________
+| Status code | Description               | Action required                 |
+--------------------------------------------------------------------------
+|     10      | Error: session encryption | The session shall be terminated.|
+|     11      | Error: CBOR decoding      | The session shall be terminated.|
+|     20      | Session termination       | The session shall be terminated.|
+--------------------------------------------------------------------------
+ */
 enum class SessionDataStatus(val value: Long) {
     ERROR_SESSION_ENCRYPTION(10L),
     ERROR_CBOR_DECODING(11L),
