433d4858c59fb72587c7f3e06af9585b80f4f7a6
LazyAfternoons <lazyafternoons@outlook.it>
2025-07-30 16:24:47 +0200

    chore: Release proximity v2.1.0

--- proximity/build.gradle.kts
+++ proximity/build.gradle.kts
@@ -40,7 +40,7 @@ android {
 }
 
 mavenPublishing {
-    coordinates("it.pagopa.io.wallet.proximity", "proximity", "2.0.0")
+    coordinates("it.pagopa.io.wallet.proximity", "proximity", "2.1.0")
 
     publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
     signAllPublications()
