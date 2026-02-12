6408c2371a30c38a8ddefb7ebe174a20ee71fbdf
Federico Mastrini <federicomastrini93@gmail.com>
2026-02-12 15:32:37 +0100

    chore: Release CBOR v1.4.0

--- cbor/build.gradle.kts
+++ cbor/build.gradle.kts
@@ -36,7 +36,7 @@ android {
 }
 
 mavenPublishing {
-    coordinates("it.pagopa.io.wallet.cbor", "cbor", "1.3.0")
+    coordinates("it.pagopa.io.wallet.cbor", "cbor", "1.4.0")
 
     publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
     signAllPublications()
