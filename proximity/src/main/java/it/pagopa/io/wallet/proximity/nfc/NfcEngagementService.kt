413cb8c70b5a736d0049bbfb8a828ce1e4ec7744
carloDigitouch <156417387+carloDigitouch@users.noreply.github.com>
2026-06-03 12:04:28 +0200

    feat: [SIW-001] solved background service starting without open app (#36)
    ## Short description
    
    Now nfc service is working just when app is opened.
    
    ## List of changes proposed in this pull request
    
    into AndroidManifest:
    ```xml
    <service
                android:name=".nfc.AppNfcEngagementService"
                android:exported="true"
                android:enabled="false"<!--This is really important!!-->
                android:label="@string/nfc_engagement_service_desc"
                android:permission="android.permission.BIND_NFC_SERVICE">
                <intent-filter>
                    <action android:name="android.nfc.cardemulation.action.HOST_APDU_SERVICE" />
                    <category android:name="android.intent.category.DEFAULT"/>
                </intent-filter>
    
                <meta-data
                    android:name="android.nfc.cardemulation.host_apdu_service"
                    android:resource="@xml/nfc_engagement_apdu_service" />
            </service>
    ```
    
    Into your activity Lifecycle:
    
    OnCreate and onPause Event:
    ```kotlin
    NfcEngagementService.disable(activity)
    ```
    
    Co-authored-by: Carlo <carloDigitouch>
    Co-authored-by: Federico Mastrini <federicomastrini93@gmail.com>

--- proximity/src/main/java/it/pagopa/io/wallet/proximity/nfc/NfcEngagementService.kt
+++ proximity/src/main/java/it/pagopa/io/wallet/proximity/nfc/NfcEngagementService.kt
@@ -105,6 +105,7 @@ abstract class NfcEngagementService : HostApduService() {
         @SuppressLint("StaticFieldLeak")
         private var activeService: NfcEngagementService? = null
         private var inactivityTimeout = 15
+        private var serviceClass: Class<out NfcEngagementService>? = null
 
         val RESPONSE_GENERATION_ERROR = NfcUtil.STATUS_WORD_FILE_NOT_FOUND
 
@@ -119,6 +120,15 @@ abstract class NfcEngagementService : HostApduService() {
             activity: Activity,
             preferredNfcEngSerCls: Class<out NfcEngagementService>? = null,
         ): HceServiceStatus {
+            serviceClass = preferredNfcEngSerCls
+            // Enable the service component programmatically
+            preferredNfcEngSerCls?.let {
+                activity.packageManager.setComponentEnabledSetting(
+                    ComponentName(activity, it),
+                    android.content.pm.PackageManager.COMPONENT_ENABLED_STATE_ENABLED,
+                    android.content.pm.PackageManager.DONT_KILL_APP
+                )
+            }
             // set preferred Nfc Engagement Service
             return preferredNfcEngSerCls?.let {
                 setAsPreferredNfcEngagementService(activity, it)
@@ -138,6 +148,15 @@ abstract class NfcEngagementService : HostApduService() {
             nfcEngagement = null
             NfcEngagementEventBus.resetSetup()
             unsetAsPreferredNfcEngagementService(activity)
+            // Disable the service component programmatically
+            serviceClass?.let {
+                activity.packageManager.setComponentEnabledSetting(
+                    ComponentName(activity, it),
+                    android.content.pm.PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
+                    android.content.pm.PackageManager.DONT_KILL_APP
+                )
+            }
+            serviceClass = null
         }
 
         private fun Int.cardEmulationClearLog() = when (this) {
